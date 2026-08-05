a65f0f49a79315fbdc903c66dfc4ab9c1d6bbaa6
batti251 <bastibuenz25@gmail.com>
2026-08-05 22:11:09 +0200

    update to angular v22

--- src/app/shared/workbook/coding/coding.ts
+++ src/app/shared/workbook/coding/coding.ts
@@ -1,30 +1,38 @@
-import { Component, inject } from '@angular/core';
-import {FormControl, FormGroup, ReactiveFormsModule, FormArray, FormBuilder, Validators, Form} from '@angular/forms';
+import { Component, inject, ChangeDetectionStrategy } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  FormArray,
+  FormBuilder,
+  Validators,
+  Form,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-coding',
   imports: [],
   templateUrl: './coding.html',
+  changeDetection: ChangeDetectionStrategy.Eager,
   styleUrl: './coding.scss',
 })
 export class Coding {
-  private formBuilder = inject(FormBuilder)
+  private formBuilder = inject(FormBuilder);
   form = this.formBuilder.group({
     language: ['', Validators.required],
     area: ['', Validators.required],
     methodFor: ['', Validators.required],
     title: ['', Validators.required],
     description: ['', Validators.required],
-    returnValue:  ['', Validators.required],
-    useCases :  this.formBuilder.array([this.formBuilder.control('')])
-  })
+    returnValue: ['', Validators.required],
+    useCases: this.formBuilder.array([this.formBuilder.control('')]),
+  });
 
-
-  get useCases(){
-    return this.form.controls.useCases as FormArray
+  get useCases() {
+    return this.form.controls.useCases as FormArray;
   }
 
-  addUseCase(){
-    this.useCases.push(this.formBuilder.control(''))
+  addUseCase() {
+    this.useCases.push(this.formBuilder.control(''));
   }
 }
