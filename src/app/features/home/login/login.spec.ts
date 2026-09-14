606c36f29ac95a1da9435ddd1142d4fa4d92be05
batti251 <bastibuenz25@gmail.com>
2026-09-14 23:32:24 +0200

    test (login): add first simple build tests

--- src/app/features/home/login/login.spec.ts
+++ src/app/features/home/login/login.spec.ts
@@ -2,21 +2,29 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { Login } from './login';
 
-describe('Login', () => {
+describe('Login (minimal)', () => {
   let component: Login;
-  let fixture: ComponentFixture<Login>;
-
+  let fixture: ComponentFixture<Login>
   beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [Login],
-    }).compileComponents();
-
     fixture = TestBed.createComponent(Login);
     component = fixture.componentInstance;
     await fixture.whenStable();
-  });
+  })
+
+  it('should contain "Email"', () => {
+    const element: HTMLElement = fixture.nativeElement;
+    expect(element.textContent).toContain('Email')
+  })
+
+  it('should have <form>', () => {
+    const element: HTMLElement = fixture.nativeElement;
+    const form = element.querySelector('form')!;
+    expect(form.textContent)
+  })
 
   it('should create', () => {
-    expect(component).toBeTruthy();
+    expect(component).toBeDefined();
   });
 });
+
+
